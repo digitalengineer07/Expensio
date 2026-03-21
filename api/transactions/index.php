a70f54845c554373e7f2bd568e9e53fcf883abb8
Nikhil kr. <166230590+digitalengineer07@users.noreply.github.com>
2026-03-21 23:36:48 +0530

    Delete api/transactions directory

--- api/transactions/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once __DIR__ . '/../../app/Controllers/TransactionController.php';
-
-use App\Controllers\TransactionController;
-
-$controller = new TransactionController();
-$controller->handleRequest();
